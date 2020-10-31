c90dca4c10decc79e4606b875f256091226705e8
NickBorgers <nickborgers@gmail.com>
2020-10-31 14:37:44 -0500

    initial commit

---
+++ apply_to_target.py
@@ -0,0 +1,65 @@
+import requests
+import os
+import json
+import argparse
+
+# Setup
+parser = argparse.ArgumentParser(description='Take a device\'s effect config and save it')
+parser.add_argument(nargs='?', dest='target', type=str, help='A device name to take config from')
+args = parser.parse_args()
+
+config_raw = open('.devices.json').read()
+config = json.loads(config_raw)
+
+if args.target is None:
+  print("Must provide a target device to pull from")
+  exit(1)
+
+target = None
+
+for device in config["devices"]:
+  if device["name"] == args.target:
+    print("Matched target: " + args.target)
+    target = device
+
+if target is None:
+  print("Did not match target: " + args.target)
+  exit(1)
+
+base_url = 'http://' + target["address"] + ':16021/api/v1/' + target["token"]
+url = base_url + '/effects'
+
+# Clear existing effects
+existing_effects_response = requests.put(url, data='{"write" : {"command" : "requestAll"}}')
+existing_effects  = json.loads(existing_effects_response.text)
+for existing_effect in existing_effects["animations"]: 
+  request = {}
+  request["write"] = {}
+  request["write"]["command"] = "delete"
+  request["write"]["animName"] = existing_effect["animName"]
+  delete_response = requests.put(url, data=json.dumps(request))
+  if delete_response.status_code >=200 and delete_response.status_code <300 :
+    print("Deleted: " + existing_effect["animName"])
+  else:
+    print("Failed to delete " + existing_effect["animName"])
+    print(delete_response)
+    exit(1)
+
+# Read effects to apply
+effects_to_apply_raw = open('effects.json').read()
+effects_to_apply = json.loads(effects_to_apply_raw)
+
+print("Read effects.json")
+
+for effect_to_apply in effects_to_apply["animations"]:
+  request = {}
+  request["write"] = effect_to_apply
+  request["write"]["command"] = "add"
+  create_response = requests.put(url, data=json.dumps(request))
+  if create_response.status_code >=200 and create_response.status_code <300 :
+    print("Create: " + effect_to_apply["animName"])
+  else:
+    print("Failed to create " + effect_to_apply["animName"])
+    print(delete_response)
+    exit(1)
+
---
+++ copy_target.py
@@ -0,0 +1,34 @@
+import requests
+import os
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Take a device\'s effect config and save it')
+parser.add_argument(nargs='?', dest='target', type=str, help='A device name to take config from')
+args = parser.parse_args()
+
+config_raw = open('.devices.json').read()
+config = json.loads(config_raw)
+
+if args.target is None:
+  print("Must provide a target device to pull from")
+  exit(1)
+
+target = None
+
+for device in config["devices"]:
+  if device["name"] == args.target:
+    print("Matched target: " + args.target)
+    target = device
+
+if target is None:
+  print("Did not match target: " + args.target)
+  exit(1)
+
+response = requests.put('http://' + target["address"] + ':16021/api/v1/' + target["token"] + '/effects', data='{"write" : {"command" : "requestAll"}}')
+
+destination_file = open('effects.json', 'w')
+destination_file.write(response.text)
+destination_file.close()
+
+print("Wrote effects.json")
